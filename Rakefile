3442354589580b56d226f96e34b1a1decbc73246
oazzat <oazzat19@gmail.com>
2018-11-14 11:19:40 -0500

    Database Populated

--- Rakefile
+++ Rakefile
@@ -9,3 +9,11 @@ task :console do
 end
 
 require_relative 'config/environment'
+
+desc 'delete all table contents'
+task :del_tbl do
+  Location.delete_all
+  Boro.delete_all
+  Zip.delete_all
+  #puts Location.count
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+require_relative "../config/environment"
+
+omar = User.create(username: "Omar")
+la = User.create(username: "La")
+carlo = User.create(username: "Carlo")
+skyler = User.create(username: "Skyler")
+user1 = User.create(username: "User1")
+user2 = User.create(username: "User2")
