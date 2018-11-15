dee7e8c090e0ee0ca99c746c2d33f6478607aacc
oazzat <oazzat19@gmail.com>
2018-11-15 17:15:01 -0500

    no change

--- bin/run.rb
+++ bin/run.rb
@@ -5,7 +5,7 @@ arter = Artii::Base.new :font => 'slant'
 puts arter.asciify("Free Public Wifi Locater")
 
 puts "Welcome to the Free Public Wifi Locater app,
-      where you can find the closet FREE public wifi location
+      where you can find the closest FREE public wifi location
       near you!!!"
 
 User.start
